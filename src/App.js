8f4e45d4317d6b67b69d44ca94ef54eb9434dc1c
oceanamassano <oshimassano@gmail.com>
2023-07-24 19:14:42 -0300

    functional without styles

--- src/App.js
+++ src/App.js
@@ -6,10 +6,11 @@ import Navbar from "./components/Navbar";
 import { useState } from "react";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(true);
+  const [darkMode, setDarkMode] = useState(false);
   return (
     <div className={darkMode && "dark"}>
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+      <main className="bg-white dark:bg-gray-900 text-gray-900 dark:text-white"></main>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { MdNightsStay, MdWbSunny } from "react-icons/md";
 
-const Navbar = () => {
+const Navbar = ({ darkMode, setDarkMode }) => {
   return (
     <header className="bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
       <nav className="flex justify-between items-center p-5">
         <h1 className="text-2xl">Oceana</h1>
-        <div>
-          <MdNightsStay className="text-3xl cursor-pointer"/>
-          <MdWbSunny className="text-3xl cursor-pointer"/>
+        <div onClick={() => setDarkMode(!darkMode)}>
+          {darkMode ? (
+            <MdWbSunny className="text-3xl cursor-pointer" />
+          ) : (
+            <MdNightsStay className="text-3xl cursor-pointer" />
+          )}
         </div>
       </nav>
     </header>
