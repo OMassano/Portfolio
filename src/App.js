081ef891793e67216a00c9e13f683b3e243152d0
oceanamassano <oshimassano@gmail.com>
2023-07-24 18:57:28 -0300

    changed local state in app

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./views/Home";
 import Landing from "./views/Landing";
 import Navbar from "./components/Navbar";
+import { useState } from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
   return (
-    <div>
-      <Navbar />
+    <div className={darkMode && "dark"}>
+      <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
     </div>
   );
 }
