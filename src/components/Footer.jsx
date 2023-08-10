df43263cdf58608bfa041880370d32fbeebce384
oceanamassano <oshimassano@gmail.com>
2023-08-10 15:28:05 -0300

    making lang button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-
+// import LangButton from "./langButton";
 import { MdNightsStay, MdWbSunny } from "react-icons/md";
 
 const Footer = ({ darkMode, setDarkMode }) => {
   return (
     <footer className="flex flex-col text-violet-500 dark:text-violet-300 w-full sm:sticky bottom-0 bg-transparent z-0">
-      <div className="justify-between items-center p-5">
+      <div className="flex justify-between  items-center p-5">
         <div onClick={() => setDarkMode(!darkMode)}>
           {darkMode ? (
             <MdWbSunny className="text-3xl cursor-pointer" />
@@ -13,6 +13,8 @@ const Footer = ({ darkMode, setDarkMode }) => {
             <MdNightsStay className="text-3xl cursor-pointer" />
           )}
         </div>
+        {/* <LangButton /> */}
+        
       </div>
     </footer>
   );
